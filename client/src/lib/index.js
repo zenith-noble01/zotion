620614ac7893ede5abb3bae7154b0938758dcad4
zenith-noble01 <webdev686@gmail.com>
2023-05-07 16:28:25 +0100

    changed the library to ts

---
+++ client/src/lib/index.js
@@ -0,0 +1,7 @@
+import SlideIn from "./components/SlideIn"
+import TextVariants from "./components/TextVariants"
+import StaggerContainer from "./components/StaggerContainer"
+import ZoomIn from "./components/ZoomIn"
+import FadeIn from "./components/FadeIn"
+
+export { SlideIn, TextVariants, StaggerContainer, ZoomIn, FadeIn }
